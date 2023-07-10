0c488b12d028294cb5d93e5a6495b5553c280616
julian <julian.bottiglione@epitech.eu>
2023-07-10 11:22:30 +0200

    [fix] SCM testing

--- get_jenkins_test.groovy
+++ get_jenkins_test.groovy
@@ -1,13 +1,9 @@
 job( 'Get Jenkins Files' ) {
-    environmentVariables(REPO: 'git@github.com:Julian52575/Jenkins.git')
+
+    environmentVariables(REPO: 'Julian52575/Jenkins.git')
 
     scm {
-        git {
-            remote {
-                name('Jenkins Files')
-                url( REPO )
-            }
-        }
+        github( '${REPO}' )
     }
-}
 
+}
