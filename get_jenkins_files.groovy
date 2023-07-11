6f65d2c9f8c32e10c8a3bf68dc872d94918fe39f
julian <julian.bottiglione@epitech.eu>
2023-07-11 11:27:12 +0200

    [add] trying chat GPT file because I have no hope left

--- get_jenkins_files.groovy
+++ get_jenkins_files.groovy
@@ -1,14 +1,33 @@
-
-job( 'Get Jenkins Files' ) {
-
-    scm {
-        github( 'Julian52575/Jenkins.git' )
+job('Fetch and Execute Groovy Files') {
+  scm {
+    git {
+      remote {
+        url('https://github.com/Julian52575/Jenkins.git')
+      }
+      branch('main') // Specify the branch you want to fetch from
     }
+  }
 
-    steps {
-        shell( 'git clone git@github.com:Julian52575/Jenkins.git a' )
-        systemGroovyCommand( 'a/.groovy*' )
+  steps {
+    script {
+      // Fetch the Groovy files from the repository
+      def groovyFiles = findFiles(glob: '**/*.groovy')
+
+      // Execute each Groovy file as a Jenkins job
+      groovyFiles.each { groovyFile ->
+        def groovyScript = readFile(groovyFile)
+        def jobName = groovyFile.name.replace('.groovy', '')
+        
+        // Create a new Jenkins job dynamically using the Groovy script
+        job(jobName) {
+          steps {
+            script {
+              // Execute the Groovy script as a Jenkins job
+              evaluate(groovyScript)
+            }
+          }
+        }
+      }
     }
-
+  }
 }
-
