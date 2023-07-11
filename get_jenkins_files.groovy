eb4acb8d6f6c3ddb92a08a2bb91725a1e555f261
julian <julian.bottiglione@epitech.eu>
2023-07-11 10:50:40 +0200

    [test] non existing repo

--- get_jenkins_files.groovy
+++ get_jenkins_files.groovy
@@ -6,7 +6,7 @@ job( 'Get Jenkins Files' ) {
     }
 
     steps {
-        shell( 'git clone git@github.com:Julian52575/Jenkins.git a' )
+        shell( 'git clone git@github.com:Julian52575/Jenkins2.git a' )
         systemGroovyCommand( 'a/dsl_math.groovy' )
         systemGroovyCommand( 'a/ls_math.groovy' )
         systemGroovyCommand( 'a/null.groovy' )
