50584734140a340454b5fde884539294f81a450e
julian <julian.bottiglione@epitech.eu>
2023-07-11 11:41:47 +0200

    [fix] fuck ChatGPT, try to make a groovy file right before trying to take over the world

--- get_jenkins_files.groovy
+++ get_jenkins_files.groovy
@@ -1,28 +1,14 @@
-job('Fetch and Execute Groovy Files') {
-  scm {
-    git {
-      remote {
-        url( 'https://github.com/Julian52575/Jenkins.git' )
-      }
-      branch('main') // Specify the branch you want to fetch from
+
+job( 'Get Jenkins Files' ) {
+
+    scm {
+        github( 'Julian52575/Jenkins.git' )
     }
-  }
 
-
-
-  steps {
-    fileOperations {
-      filesMatching('**/*.groovy') {
-        def groovyFile = it.path
-        def groovyScript = readFile(groovyFile)
-        def jobName = groovyFile.replace('.groovy', '')
-
-        job(jobName) {
-          steps {
-            systemGroovyScript(groovyScript)
-          }
-        }
-      }
+    steps {
+        shell( 'git clone git@github.com:Julian52575/Jenkins.git a' )
+        systemGroovyCommand( 'a/.groovy*' )
     }
-  }
+
 }
+
